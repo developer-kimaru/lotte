44b6729aaaf42fb76afe8f1cdc2608a39d1c43de
YS <develop-kimaru>
2021-04-02 10:17:35 +0900

    2021-04-02 / 10:17

---
+++ Day0329/src/net/hb/day29/test.java
@@ -0,0 +1,14 @@
+package net.hb.day29;
+
+public class test {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for (int a=0; a<10; a++) {
+			i = i+1;
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
